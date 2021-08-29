902e8e97d3a339fda932efd553c4b4f229dd72ba
He Wu(Heva) <hewu9375@gmail.com>
2021-08-29 21:01:11 +0900

    Day16: init project

---
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by He Wu on 2021/08/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
