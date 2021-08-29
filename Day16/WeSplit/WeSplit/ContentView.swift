a31672f596b92fd30d808df68031dd1c4eeb8419
He Wu(Heva) <hewu9375@gmail.com>
2021-08-29 21:25:35 +0900

    Day16: use form

--- Day16/WeSplit/WeSplit/ContentView.swift
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // Form have limitation on 10
+        Form {
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
     }
 }
 
