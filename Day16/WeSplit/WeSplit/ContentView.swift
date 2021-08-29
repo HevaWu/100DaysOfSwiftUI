858eb8847b25bf3d9a8baccd29d16d404360b531
He Wu(Heva) <hewu9375@gmail.com>
2021-08-29 21:36:31 +0900

    Day16: Use plain string in navigationbar

--- Day16/WeSplit/WeSplit/ContentView.swift
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -35,7 +35,7 @@ struct ContentView: View {
                     Text("Hello, world!")
                 }
             }
-            .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
+            .navigationBarTitle(Text("SwiftUI"))
         }
     }
 }
