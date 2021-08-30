514d7783543dc03285e4f73387530e98e634a447
He Wu(Heva) <hewu9375@gmail.com>
2021-08-30 21:19:41 +0900

    Day17: Section header & picker style

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -28,6 +28,15 @@ struct ContentView: View {
                     }
                 }
                 
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
                 Section {
                     Text("$\(checkAmount)")
                 }
