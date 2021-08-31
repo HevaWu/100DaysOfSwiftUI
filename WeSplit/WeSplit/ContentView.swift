f994ee82e4d2d123527dce76c82019b61a56959c
He Wu(Heva) <hewu9375@gmail.com>
2021-08-31 21:02:59 +0900

    Day18: add header to 3rd section "Amount Per person"

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -48,7 +48,7 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section {
+                Section(header: Text("Amount per person")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
