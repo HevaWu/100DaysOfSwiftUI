38ba5e5e6986fe3e950f31c44e47a01fc4b5fcd2
He Wu(Heva) <hewu9375@gmail.com>
2021-09-03 19:47:12 +0900

    Day21: add Flags and VStack for showing them

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,16 +10,43 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingAlert = false
     
+    var countries = [
+        "Estonia",
+        "France",
+        "Germany",
+        "Ireland",
+        "Italy",
+        "Nigeria",
+        "Poland",
+        "Russia",
+        "Spain",
+        "UK",
+        "US"
+    ]
+    
+    var correctIndex = Int.random(in: 0...2)
+    
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
-        }
-        .alert(isPresented: $showingAlert) {
-            Alert(
-                title: Text("Title"),
-                message: Text("message"),
-                dismissButton: .default(Text("Button"))
-            )
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctIndex])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        // flag was tapped
+                    }, label: {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    })
+                }
+            }
         }
     }
 }
