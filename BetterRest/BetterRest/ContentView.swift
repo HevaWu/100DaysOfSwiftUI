3328bdfa5fa6f1ec9d79028e5a589b85e7420b44
He Wu(Heva) <hewu9375@gmail.com>
2021-09-08 21:23:37 +0900

    Day26: init project

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by He Wu on 2021/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
