4cc120e380d58639bb5a1dc4146ac86896133dc5
He Wu(Heva) <hewu9375@gmail.com>
2021-09-08 21:54:36 +0900

    Day26: CreateML to train BetterRest model

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -11,21 +11,7 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     
     var body: some View {
-//        var components = DateComponents()
-//        components.hour = 9
-//        components.minute = 0
-//
-//        let date = Calendar.current.date(from: components) ?? Date()
-        
-//        let components = Calendar.current.dateComponents([.hour, .minute], from: Date())
-//        let hour = components.hour ?? 0
-//        let minute = components.minute ?? 0
-        
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        let dateString = formatter.string(from: Date())
-        
-        return  DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
             .labelsHidden()
     }
 }
