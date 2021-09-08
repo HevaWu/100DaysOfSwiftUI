ac7e94958ce4466898dadf09f5fa3972301f8ce3
He Wu(Heva) <hewu9375@gmail.com>
2021-09-08 21:38:48 +0900

    Day26: DateComponents + DateFormatter

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     
     var body: some View {
-        let now = Date()
-        let tomorrow = Date().addingTimeInterval(86400)
-        let range = now...tomorrow
+//        var components = DateComponents()
+//        components.hour = 9
+//        components.minute = 0
+//
+//        let date = Calendar.current.date(from: components) ?? Date()
         
-        DatePicker("Please enter a date", selection: $wakeUp, in: range)
+//        let components = Calendar.current.dateComponents([.hour, .minute], from: Date())
+//        let hour = components.hour ?? 0
+//        let minute = components.minute ?? 0
+        
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        let dateString = formatter.string(from: Date())
+        
+        return  DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
             .labelsHidden()
     }
 }
