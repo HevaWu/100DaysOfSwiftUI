8f7a43075a82d0dcc14321f582c730c4fec6dd53
He Wu(Heva) <hewu9375@gmail.com>
2021-09-10 21:56:45 +0900

    Day28: use Picker to show daily coffee intake

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -39,13 +39,16 @@ struct ContentView: View {
                 }
                 
                 Section(header: Text("Daily coffee intake")) {
-                    Stepper(value: $coffeeAmount, in: 1...20) {
-                        if coffeeAmount == 1 {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeeAmount) cups")
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) { cup in
+                            if cup == 1 {
+                                Text("1 cup")
+                            } else {
+                                Text("\(cup) cups")
+                            }
                         }
                     }
+                    .pickerStyle(InlinePickerStyle())
                 }
             }
             .navigationTitle("BetterRest")
