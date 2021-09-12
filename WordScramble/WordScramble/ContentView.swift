868aa2b5a8665a3c51a48faaceb28cb3becc94c4
He Wu(Heva) <hewu9375@gmail.com>
2021-09-12 20:07:01 +0900

    Day30: add validation and shown error if there is any miss

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showError = false
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -27,7 +31,44 @@ struct ContentView: View {
             }
             .navigationBarTitle(rootWord)
             .onAppear(perform: startGame)
+            .alert(isPresented: $showError, content: {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            })
         }
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showError = true
+    }
+    
+    // is atualy english word or not
+    func isRealWord(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelled = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelled.location == NSNotFound
+    }
+    
+    // not trying to spell "car" from `silkworm`
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord.lowercased()
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    // word hasn't been used
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
     }
     
     func startGame() {
@@ -45,6 +86,22 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard !answer.isEmpty else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "Cannot make up")
+            return
+        }
+        
+        guard isRealWord(word: answer) else {
+            wordError(title: "Word not possible", message: "Not a real word")
+            return
+        }
+        
         usedWords.insert(answer, at: 0)
         newWord = ""
     }
