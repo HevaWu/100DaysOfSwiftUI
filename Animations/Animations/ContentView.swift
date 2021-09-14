4f5efa37bd6c78e596f5a26afd8be14e58d51595
He Wu(Heva) <hewu9375@gmail.com>
2021-09-14 21:58:51 +0900

    Day32: bind animationAmount and animate it

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,26 +11,28 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
     
     var body: some View {
-        Button("Tap Me") {
-//            self.animationAmount += 1
-        }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(
+        print(animationAmount)
+        
+        return VStack {
+            Stepper(
+                "Scale amount",
+                value: $animationAmount.animation(
                     Animation.easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false)
-                )
-        )
-        .onAppear(perform: {
-            self.animationAmount = 2
-        })
+                        .repeatCount(3, autoreverses: true)
+                ),
+                in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
     }
 }
 
