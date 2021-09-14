b76a7af5749797a2b7c353705a2e5dbe21981e8a
He Wu(Heva) <hewu9375@gmail.com>
2021-09-14 21:35:00 +0900

    Day32: animation with scale+blur

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            self.animationAmount += 1
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount-1) * 3)
+        .animation(.default)
     }
 }
 
