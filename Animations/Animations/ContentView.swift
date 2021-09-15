8a0e15eee6f0c6babbd0b86e7199467559777256
He Wu(Heva) <hewu9375@gmail.com>
2021-09-15 21:51:33 +0900

    Day33: animate drag gesture of letters

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,27 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        LinearGradient(
-            gradient: Gradient(colors: [.yellow, .red]),
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
-        .frame(width: 300, height: 200)
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .offset(dragAmount)
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? Color.blue : Color.red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20))
+            }
+        }
         .gesture(
             DragGesture()
-                .onChanged({
+                .onChanged {
                     dragAmount = $0.translation
-                })
-                .onEnded({ _ in
-                    withAnimation(.spring()) {
-                        dragAmount = .zero
-                    }
-                })
+                }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
         )
     }
 }
