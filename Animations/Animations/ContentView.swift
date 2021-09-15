154fc179796e5a2ed4e4fb950f4c3339b1d5807f
He Wu(Heva) <hewu9375@gmail.com>
2021-09-15 21:58:58 +0900

    Day33: apply transition to a View

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,32 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? Color.blue : Color.red)
-                    .offset(dragAmount)
-                    .animation(.default.delay(Double(num) / 20))
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged {
-                    dragAmount = $0.translation
-                }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
     }
 }
 
