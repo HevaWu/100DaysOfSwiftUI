df2d6faf2627466f8ac4821c9401654be1ceb90c
He Wu(Heva) <hewu9375@gmail.com>
2021-09-17 20:36:26 +0900

    Day35: init project

---
+++ Edutainment/Edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by He Wu on 2021/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
