e6b0409063ceec51bc9998ac4e5d0c6a893d9b30
He Wu(Heva) <hewu9375@gmail.com>
2021-09-17 21:17:24 +0900

    Day35: show first Set up question view

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var difficult = 5
+    @State private var questionCount = 3
+    
+    @State private var showSettingView = true
+    
+    var questionCountList = ["5", "10", "20", "ALL"]
+    
+    var settingView: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Define Difficulty")) {
+                        Stepper("Up to \(difficult)", value: $difficult, in: 1...12)
+                    }
+                    
+                    Section(header: Text("How many questions?")) {
+                        
+                        Picker("Question Count", selection: $questionCount) {
+                            ForEach(0..<questionCountList.count) {
+                                Text("\(questionCountList[$0])")
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+                
+                Button(action: {
+                    startGame()
+                }, label: {
+                    Text("Start Game!")
+                })
+                .font(.title)
+                .frame(width: 200, height: 200)
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                
+                Spacer()
+                    .frame(maxHeight: .infinity)
+            }
+            .navigationTitle("Set up Your Game")
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        settingView
+    }
+    
+    func startGame() {
+        print("tapped start game!")
     }
 }
 
