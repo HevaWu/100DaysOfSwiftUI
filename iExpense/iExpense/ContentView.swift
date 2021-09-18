9964c74c0a0e0583ff6e52f30c137c50949a9dd6
He Wu(Heva) <hewu9375@gmail.com>
2021-09-18 22:31:02 +0900

    Day36: Codable + JSONEncoder

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var first: String
+    var second: String
+}
+
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @State private var user = User(first: "Alice", second: "Bob")
     
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
-            UserDefaults.standard.setValue(tapCount, forKey: "Tap")
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.setValue(data, forKey: "UserData")
+            }
         }
     }
 }
