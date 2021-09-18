abef7edf4a7fba9932df37edd4bebd5078bea7fb
He Wu(Heva) <hewu9375@gmail.com>
2021-09-18 21:37:43 +0900

    Day36: init project

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by He Wu on 2021/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
