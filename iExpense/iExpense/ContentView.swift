dc51354776f6533349f898be97321a792cf8abfb
He Wu(Heva) <hewu9375@gmail.com>
2021-09-19 19:31:11 +0900

    Day37: create ExpenseItem, Expenses, and add + delete function

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,21 +7,44 @@
 
 import SwiftUI
 
-struct User: Codable {
-    var first: String
-    var second: String
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
 }
 
 struct ContentView: View {
-    @State private var user = User(first: "Alice", second: "Bob")
-    
+    @ObservedObject var expenses = Expenses()
+
     var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.setValue(data, forKey: "UserData")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: { indexSet in
+                    removeItems(at: indexSet)
+                })
             }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(
+                trailing:
+                    Button(action: {
+                        let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                        expenses.items.append(expense)
+                    }, label: {
+                        Image(systemName: "plus")
+                    })
+            )
         }
+    }
+    
+    func removeItems(at offset: IndexSet) {
+        expenses.items.remove(atOffsets: offset)
     }
 }
 
