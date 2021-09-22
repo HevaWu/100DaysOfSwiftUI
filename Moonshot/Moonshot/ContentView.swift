c8f206ca805a444f27510111817f49558856bdd3
He Wu(Heva) <hewu9375@gmail.com>
2021-09-22 22:57:19 +0900

    Day39: aspectRatio

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("cat")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+        }
     }
 }
 
