34f8f85a3eee5ec87bedf893b4017e004b350b58
He Wu(Heva) <hewu9375@gmail.com>
2021-09-25 16:36:10 +0900

    Day43: draw triangle

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+        }
+        .stroke(Color.blue.opacity(0.25), lineWidth: 10)
     }
 }
 
