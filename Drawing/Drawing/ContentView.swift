0e05e12374570398e480bbb890d14df93d9a2721
He Wu(Heva) <hewu9375@gmail.com>
2021-09-26 15:29:39 +0900

    Day44: make color cycling circle

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
     var body: some View {
-        Capsule()
-            .strokeBorder(
-                ImagePaint(
-                    image: Image("bear"),
-                    scale: 1),
-                lineWidth: 30)
-            .frame(width: 300, height: 200)
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(
+                        self.color(for: value, brightness: 1),
+                        lineWidth: 2
+                    )
+                
+            }
+        }
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) * self.amount
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ContentView: View {
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 
