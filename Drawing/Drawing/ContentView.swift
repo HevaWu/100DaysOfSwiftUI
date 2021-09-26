ea206eb6ea6349f9a15210b7a246fa89e8392b62
He Wu(Heva) <hewu9375@gmail.com>
2021-09-26 15:15:26 +0900

    Day44: apply Image border with ImagePaint

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,48 +7,17 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset: Double = -20
-    var petalWidth: Double = 100
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        // 16 petals
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            // each petal, draw ellipse
-            let rotation = CGAffineTransform(rotationAngle: number)
-            
-            // move petal to be at the center of the view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width/2))
-            
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        return path
-    }
-}
-
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-    
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(Color.blue, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .vertical])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding([.horizontal, .vertical])
-        }
+        Text("Hello World")
+            .frame(width: 300, height: 300)
+            .border(
+                ImagePaint(
+                    image: Image("bear"),
+                    sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5),
+                    scale: 1.0),
+                width: 30
+            )
     }
 }
 
