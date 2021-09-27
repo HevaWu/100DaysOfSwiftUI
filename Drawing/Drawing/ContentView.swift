f7feb023871e89a4e17abfcf4c524fa56de73a64
He Wu(Heva) <hewu9375@gmail.com>
2021-09-27 22:17:06 +0900

    Day45: fancy Spirograph (Hypotrochoid)

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,35 +7,50 @@
 
 import SwiftUI
 
-struct Checkerboard: Shape {
-    var rows: Int
-    var cols: Int
+// https://en.wikipedia.org/wiki/Hypotrochoid
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
     
-    public var animatableData: AnimatablePair<Double, Double> {
-        get {
-            AnimatablePair(Double(rows), Double(cols))
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
         }
-        set {
-            self.rows = Int(newValue.first)
-            self.cols = Int(newValue.second)
-        }
+        
+        return a
     }
     
     func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        
+        let outerRadius = CGFloat(outerRadius)
+        let innerRadius = CGFloat(innerRadius)
+        let distance = CGFloat(distance)
+        
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
+        
         var path = Path()
         
-        let rowSize = rect.height / CGFloat(rows)
-        let colSize = rect.width / CGFloat(cols)
-        
-        for r in 0..<rows {
-            for c in 0..<cols {
-                if (r + c).isMultiple(of: 2) {
-                    let startX = colSize * CGFloat(c)
-                    let startY = rowSize * CGFloat(r)
-                    
-                    let rect = CGRect(x: startX, y: startY, width: colSize, height: rowSize)
-                    path.addRect(rect)
-                }
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
             }
         }
         
@@ -44,17 +59,45 @@ struct Checkerboard: Shape {
 }
 
 struct ContentView: View {
-    @State private var rows = 4
-    @State private var cols = 4
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    
+    @State private var hue = 0.4
     
     var body: some View {
-        Checkerboard(rows: rows, cols: cols)
-            .onTapGesture {
-                withAnimation(.linear(duration: 3)) {
-                    self.rows = 8
-                    self.cols = 16
-                }
+        VStack(spacing: 0) {
+            Spacer()
+            
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Group {
+                Text("Inner Radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer Radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 11...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Amount: \(amount, specifier: "%.2f")")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                    .padding([.horizontal])
             }
+        }
     }
 }
 
