c8d63c64bceb3afe8c50b5423f2671958d130465
He Wu(Heva) <hewu9375@gmail.com>
2021-09-29 21:27:44 +0900

    Day47: init project

---
+++ HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by He Wu on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
