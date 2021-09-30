94f8eae28e930b628f24703e304458013970a6ee
He Wu(Heva) <hewu9375@gmail.com>
2021-09-30 20:43:32 +0900

    Day49: disabled() to disable part of section

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,50 +7,30 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        }
-        .onAppear(perform: loadData)
-    }
-    
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data, let decodeResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                DispatchQueue.main.async {
-                    self.results = decodeResponse.results
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account")
                 }
-                return
             }
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            .disabled(username.isEmpty || email.isEmpty || disableForm)
         }
-        .resume()
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
