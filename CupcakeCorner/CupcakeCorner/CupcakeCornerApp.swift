13351b5583c5df1b61f01be1917416dad8ba2abd
He Wu(Heva) <hewu9375@gmail.com>
2021-09-30 20:17:37 +0900

    Day49: init project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by He Wu on 2021/09/30.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
