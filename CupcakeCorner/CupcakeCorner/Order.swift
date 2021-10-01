6349c4a6e16402fac303a26e7490ee5088ceb96f
He Wu(Heva) <hewu9375@gmail.com>
2021-10-01 20:42:06 +0900

    Day50: when specialRequestEnabled is false, reset other 2 special request

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -13,7 +13,14 @@ class Order: ObservableObject {
     @Published var type = 0
     @Published var quantity = 3
     
-    @Published var specialRequestEnabled = false
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
     @Published var extraFrosting = false
     @Published var addSprinkles = false
 }
