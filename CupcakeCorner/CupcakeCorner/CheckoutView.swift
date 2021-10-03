c3775d22a7e554710cd3e0b478ee3971740a33b0
He Wu(Heva) <hewu9375@gmail.com>
2021-10-03 18:15:02 +0900

    Day52: Make Order as struct, and have OrderViewModel to check its data

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct CheckoutView: View {
-    @ObservedObject var order: Order
+    @ObservedObject var order: OrderViewModel
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
@@ -40,7 +40,7 @@ struct CheckoutView: View {
     }
     
     func placeOrder() {
-        guard let encoded = try? JSONEncoder().encode(order) else {
+        guard let encoded = try? JSONEncoder().encode(order.order) else {
             print("Failed to encode order.")
             return
         }
@@ -74,6 +74,6 @@ struct CheckoutView: View {
 
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        CheckoutView(order: Order())
+        CheckoutView(order: OrderViewModel())
     }
 }
--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-class Order: ObservableObject, Codable {
+struct Order: Codable {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
-    @Published var type = 0
-    @Published var quantity = 3
+    var type = 0
+    var quantity = 3
     
-    @Published var specialRequestEnabled = false {
+    var specialRequestEnabled = false {
         didSet {
             if specialRequestEnabled == false {
                 extraFrosting = false
@@ -21,75 +21,41 @@ class Order: ObservableObject, Codable {
             }
         }
     }
-    @Published var extraFrosting = false
-    @Published var addSprinkles = false
+    var extraFrosting = false
+    var addSprinkles = false
     
-    @Published var name = ""
-    @Published var streetAddress = ""
-    @Published var city = ""
-    @Published var zip = ""
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+}
+
+class OrderViewModel: ObservableObject {
+    
+    @Published var order: Order = Order()
     
     var hasValidAddress: Bool {
-        if name.trimmingCharacters(in: .whitespaces).isEmpty
-            || streetAddress.trimmingCharacters(in: .whitespaces).isEmpty
-            || city.trimmingCharacters(in: .whitespaces).isEmpty
-            || zip.trimmingCharacters(in: .whitespaces).isEmpty {
+        if order.name.trimmingCharacters(in: .whitespaces).isEmpty
+            || order.streetAddress.trimmingCharacters(in: .whitespaces).isEmpty
+            || order.city.trimmingCharacters(in: .whitespaces).isEmpty
+            || order.zip.trimmingCharacters(in: .whitespaces).isEmpty {
             return false
         }
         return true
     }
     
     var cost: Double {
-        var cost = Double(quantity) * 2
-        cost += Double(type) / 2
+        var cost = Double(order.quantity) * 2
+        cost += Double(order.type) / 2
         
-        if extraFrosting {
-            cost += Double(quantity)
+        if order.extraFrosting {
+            cost += Double(order.quantity)
         }
         
-        if addSprinkles {
-            cost += Double(quantity) / 2
+        if order.addSprinkles {
+            cost += Double(order.quantity) / 2
         }
         
         return cost
-    }
-    
-    enum CodingKeys: CodingKey {
-        case type
-        case quantity
-        case extraFrosting
-        case addSprinkles
-        case name
-        case streetAddress
-        case city
-        case zip
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(type, forKey: .type)
-        try container.encode(quantity, forKey: .quantity)
-        try container.encode(extraFrosting, forKey: .extraFrosting)
-        try container.encode(addSprinkles, forKey: .addSprinkles)
-        try container.encode(name, forKey: .name)
-        try container.encode(streetAddress, forKey: .streetAddress)
-        try container.encode(city, forKey: .city)
-        try container.encode(zip, forKey: .zip)
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        type = try container.decode(Int.self, forKey: .type)
-        quantity = try container.decode(Int.self, forKey: .quantity)
-        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
-        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
-        name = try container.decode(String.self, forKey: .name)
-        streetAddress = try container.decode(String.self, forKey: .streetAddress)
-        city = try container.decode(String.self, forKey: .city)
-        zip = try container.decode(String.self, forKey: .zip)
-    }
-    
-    init() {
-
     }
 }
