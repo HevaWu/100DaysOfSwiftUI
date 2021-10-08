cc4ea3d50c5db044192f87cb00cc5ca6c0ec8cd4
He Wu(Heva) <hewu9375@gmail.com>
2021-10-08 21:26:23 +0900

    Day57: create manual managed subclass object

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by He Wu on 2021/10/08.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
