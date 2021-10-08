efb44fa087ae971b4d12bfdfe0d78d42da0aa5bd
He Wu(Heva) <hewu9375@gmail.com>
2021-10-08 21:06:04 +0900

    Day57: init project

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by He Wu on 2021/10/08.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
