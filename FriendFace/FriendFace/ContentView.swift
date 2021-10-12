96ff6d91c6ecaf79e74368c2cfcfbb86cc82e88d
He Wu(Heva) <hewu9375@gmail.com>
2021-10-12 22:38:34 +0900

    Day61: add clear + load button, check when to load user from store, when from web

--- FriendFace/FriendFace/ContentView.swift
+++ FriendFace/FriendFace/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: DownloadedUser.entity(), sortDescriptors: []) var downloadedUsers: FetchedResults<DownloadedUser>
+    
     @State private var users = [User]()
 
     var body: some View {
@@ -22,10 +25,53 @@ struct ContentView: View {
             }
             .onAppear(perform: loadUsers)
             .navigationBarTitle(Text("User List"))
+            .navigationBarItems(
+                leading: Button("Clear All Data", action: {
+                    clearAllData()
+                }),
+                trailing: Button("Load User from web", action: {
+                    loadUserFromWeb()
+                })
+            )
+        }
+    }
+    
+    func clearAllData() {
+        do {
+            for user in downloadedUsers {
+                for tag in user.wrappedTags {
+                    moc.delete(tag)
+                }
+                
+                for friend in user.wrappedFriends {
+                    moc.delete(friend)
+                }
+                moc.delete(user)
+            }
+            try moc.save()
+            
+            loadUserFromDataStore()
+        } catch {
+            print("[ContentView] remove user failed. \(error.localizedDescription)")
         }
     }
     
     func loadUsers() {
+        if !downloadedUsers.isEmpty {
+            loadUserFromDataStore()
+            return
+        }
+        
+        loadUserFromWeb()
+    }
+    
+    func loadUserFromDataStore() {
+        users = downloadedUsers
+            .map { $0.convertToUser() }
+        print("[ContentView] load from dataStore.")
+    }
+    
+    func loadUserFromWeb() {
         guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
             print("[ContentView] load users failed: invalid URL")
             return
@@ -35,6 +81,7 @@ struct ContentView: View {
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data,
                let decoded = try? JSONDecoder().decode([User].self, from: data) {
+                saveToDataStore(decoded)
                 users = decoded
             } else {
                 print("[ContentView] load users failed: response error \(error?.localizedDescription ?? "Unknown Error")")
@@ -42,15 +89,41 @@ struct ContentView: View {
         }
         .resume()
     }
-
+    
+    func saveToDataStore(_ decodedUsers: [User]) {
+        do {
+            for user in decodedUsers {
+                let newUser = DownloadedUser(context: moc)
+                newUser.id = user.id
+                newUser.isActive = user.isActive
+                newUser.name = user.name
+                newUser.age = Int16(user.age)
+                newUser.company = user.company
+                newUser.email = user.email
+                newUser.address = user.address
+                newUser.about = user.about
+                newUser.registered = user.registered
+                
+                for tag in user.tags {
+                    let newTag = DownloadedTag(context: moc)
+                    newTag.name = tag
+                    newUser.addToTags(newTag)
+                }
+                
+                for friend in user.friends {
+                    let newFriend = DownloadedFriend(context: moc)
+                    newFriend.id = friend.id
+                    newFriend.name = friend.name
+                    newUser.addToFriends(newFriend)
+                }
+            }
+            
+            try moc.save()
+        } catch {
+            print("[ContentView] Save data error. \(error.localizedDescription)")
+        }
+    }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
