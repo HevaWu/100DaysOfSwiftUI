ca1b482a29765b761d62d72e8112a372f081b3c7
He Wu(Heva) <hewu9375@gmail.com>
2021-10-20 21:35:03 +0900

    Day69: use coordinator to implement MKMapViewDelegate

--- BucketList/BucketList/MapView.swift
+++ BucketList/BucketList/MapView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
     func updateUIView(_ uiView: MKMapView, context: Context) {
         
     }
     
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
         return mapView
     }
 }
