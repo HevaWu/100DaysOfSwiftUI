b6a62f0bc2add4562f70078fb7ce455d204081b1
He Wu(Heva) <hewu9375@gmail.com>
2021-10-22 20:34:12 +0900

    Day71: when load Edit View show nearby places

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 import MapKit
 
+enum LoadingState {
+    case loading
+    case loaded
+    case failed
+}
+
 struct EditView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var placemark: MKPointAnnotation
+    
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
     
     var body: some View {
         NavigationView {
@@ -19,12 +28,53 @@ struct EditView: View {
                     TextField("Place name", text: $placemark.wrappedTitle)
                     TextField("Description", text: $placemark.wrappedSubtitle)
                 }
+                
+                Section(header: Text("Nearby...")) {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading...")
+                    case .loaded:
+                        List(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(":")
+                            + Text("Page description here")
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Please try again later")
+                    }
+                }
             }
             .navigationBarTitle("Edit Place")
             .navigationBarItems(trailing: Button("Done") {
                 self.presentationMode.wrappedValue.dismiss()
             })
+            .onAppear(perform: fetchNearbyPlaces)
         }
+    }
+    
+    func fetchNearbyPlaces() {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                if let items = try? decoder.decode(Result.self, from: data) {
+                    self.pages = Array(items.query.pages.values)
+                    self.loadingState = .loaded
+                    return
+                }
+            }
+            
+            self.loadingState = .failed
+        }
+        .resume()
     }
 }
 
---
+++ BucketList/BucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by He Wu on 2021/10/22.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
