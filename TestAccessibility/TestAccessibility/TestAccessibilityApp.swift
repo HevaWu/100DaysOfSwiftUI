d11be68734d04168328303f3f07504edfeaa8ab8
He Wu(Heva) <hewu9375@gmail.com>
2021-10-27 21:36:51 +0900

    Day74: init project

---
+++ TestAccessibility/TestAccessibility/TestAccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAccessibilityApp.swift
+//  TestAccessibility
+//
+//  Created by He Wu on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct TestAccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
