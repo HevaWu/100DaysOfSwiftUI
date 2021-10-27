80bc7dea9ccd96bc313bb861a6d89c2133c72653
He Wu(Heva) <hewu9375@gmail.com>
2021-10-27 21:58:46 +0900

    Day74: identify voiceOver label by using accessibilityLabel, accessibilityAddTraits, accessibilityRemoveTraits

--- TestAccessibility/TestAccessibility/ContentView.swift
+++ TestAccessibility/TestAccessibility/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .accessibilityLabel(Text(labels[selectedPicture]))
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
+            .onTapGesture {
+                self.selectedPicture = Int.random(in: 0...3)
+            }
     }
 }
 
