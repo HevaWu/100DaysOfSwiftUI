8e770e09b1c2da6b5b1cd46ee6d438346d33b7a7
He Wu(Heva) <hewu9375@gmail.com>
2021-11-03 21:05:53 +0900

    Day79: programmatically switch tab in TabView

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,18 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = 1
+                }
                 .tabItem {
                     Image(systemName: "star")
                     Text("One")
                 }
+                .tag(0)
+            
             Text("Tab 2")
                 .tabItem {
                     Image(systemName: "star.fill")
                     Text("Two")
                 }
+                .tag(1)
         }
     }
 }
