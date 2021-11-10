2d9907b1c6687d4b527d3f1697dccfa7c1f76e3c
He Wu(Heva) <hewu9375@gmail.com>
2021-11-10 22:15:05 +0900

    Day84: schedule notification for not contact prospect

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -57,6 +57,12 @@ struct ProspectsView: View {
                         Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted") {
                             prospects.toggle(prospect)
                         }
+                        
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                addNotification(for: prospect)
+                            }
+                        }
                     }
                 }
             }
@@ -88,6 +94,40 @@ struct ProspectsView: View {
             prospects.add(person)
         case .failure(let error):
             print("Scanning Failed \(error.localizedDescription)")
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+//            var dateComponents = DateComponents()
+//            dateComponents.hour = 9
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Fail")
+                    }
+                }
+            }
         }
     }
 }
