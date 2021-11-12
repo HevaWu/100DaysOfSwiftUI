27f1f6ccf53e18b90f3069a2c5382b8888934d1a
He Wu(Heva) <hewu9375@gmail.com>
2021-11-12 22:53:39 +0900

    Day86: onLongPressGesture

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .onTapGesture(count: 2) {
-                print("Double tapped")
+            .onLongPressGesture(minimumDuration: 2) {
+                print("Long Pressed")
             }
     }
 }
