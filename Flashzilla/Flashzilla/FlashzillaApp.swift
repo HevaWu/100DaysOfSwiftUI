9ff64b295aac8b2952a0475529faff9d6fb11528
He Wu(Heva) <hewu9375@gmail.com>
2021-11-12 22:48:06 +0900

    Day86: init project

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by He Wu on 2021/11/12.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
