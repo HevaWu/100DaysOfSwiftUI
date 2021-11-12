1119e8ac9f72862b67a557ce7151ffa18ed56649
He Wu(Heva) <hewu9375@gmail.com>
2021-11-12 22:51:05 +0900

    Day86: double tap by onTapGesture(count:2)

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onTapGesture(count: 2) {
+                print("Double tapped")
+            }
     }
 }
 
