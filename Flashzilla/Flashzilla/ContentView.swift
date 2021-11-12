27c1caa2025be14d1a3bc0b1f10cc1deac79d8eb
He Wu(Heva) <hewu9375@gmail.com>
2021-11-12 22:56:09 +0900

    Day86: onLongTapGesture pressing handler

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .onLongPressGesture(minimumDuration: 2) {
+            .onLongPressGesture(minimumDuration: 1, pressing: { inProgress in
+                print("In progress: \(inProgress)")
+            }) {
                 print("Long Pressed")
             }
     }
