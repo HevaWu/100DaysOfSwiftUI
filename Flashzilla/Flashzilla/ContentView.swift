858429f70cb50d4a9843e34532613f5305a6e351
He Wu(Heva) <hewu9375@gmail.com>
2021-11-12 23:01:59 +0900

    Day86: gesture() with MagnificationGesture

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount: CGFloat = 0
+    @State private var finalAmount: CGFloat = 1
+    
     var body: some View {
         Text("Hello, world!")
-            .onLongPressGesture(minimumDuration: 1, pressing: { inProgress in
-                print("In progress: \(inProgress)")
-            }) {
-                print("Long Pressed")
-            }
+            .scaleEffect(finalAmount + currentAmount)
+            .gesture(
+                MagnificationGesture()
+                    .onChanged({ amount in
+                        currentAmount = amount - 1
+                    })
+                    .onEnded({ amount in
+                        finalAmount += currentAmount
+                        currentAmount = 0
+                    })
+            )
     }
 }
 
