75ec9784d40483f0a4efebfa9e28f3089c703a18
He Wu(Heva) <hewu9375@gmail.com>
2021-11-14 19:23:12 +0900

    Day88: add removal callback of CardView, then call it with withAnimation to see remaining automatically slide up

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
+    
     @State private var isShowAnswer = false
     @State private var offset = CGSize.zero
 
@@ -44,6 +46,7 @@ struct CardView: View {
                 .onEnded { _ in
                     if abs(offset.width) > 100 {
                         // remove the card
+                        removal?()
                     } else {
                         offset = .zero
                     }
