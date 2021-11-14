1a78e3ea73e28963a868721177804b0047423387
He Wu(Heva) <hewu9375@gmail.com>
2021-11-14 19:19:22 +0900

    Day88: drag card , and show effect. order is rotation -> offset -> opacity -> drag

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CardView: View {
     let card: Card
     @State private var isShowAnswer = false
+    @State private var offset = CGSize.zero
 
     var body: some View {
         ZStack {
@@ -32,6 +33,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        // remove the card
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowAnswer.toggle()
         }
