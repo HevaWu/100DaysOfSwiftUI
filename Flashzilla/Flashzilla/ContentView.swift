6ecdbe9d7373b6896df05e7382127910162420cb
He Wu(Heva) <hewu9375@gmail.com>
2021-11-14 19:06:46 +0900

    Day88: add stack of card view

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
+    @State private var cards: [Card] = Array(repeating: Card.example, count: 10)
+    
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
     }
 }
