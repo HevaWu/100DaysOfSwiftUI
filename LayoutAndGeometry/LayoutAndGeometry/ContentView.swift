17bd9b0205361880678dde5756d95df54e74beb7
He Wu(Heva) <hewu9375@gmail.com>
2021-11-18 22:22:26 +0900

    Day92: HStack(alignment: .lastTextBaseline)

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Live long and prosper")
-            .frame(width: 300, height: 300, alignment: .topLeading)
+        HStack(alignment: .lastTextBaseline) {
+            Text("Live")
+                .font(.caption)
+            Text("Long")
+            Text("and")
+                .font(.title)
+            Text("prosper")
+                .font(.largeTitle)
+        }
     }
 }
 
