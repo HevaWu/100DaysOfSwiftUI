bef78c066d21bda2ff0167c55a97377225370c9a
He Wu(Heva) <hewu9375@gmail.com>
2021-11-18 22:13:49 +0900

    Day92: wholly layout neutral

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding(20)
-            .background(.red)
+        Color.red
     }
 }
 
