861ecd6193a915ec35e027634913282b15c9f122
He Wu(Heva) <hewu9375@gmail.com>
2021-11-18 21:55:04 +0900

    Day92: init project

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by He Wu on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
