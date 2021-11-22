f1b78d675edb98aff96b1ee8c94be2d71c7d5aef
He Wu(Heva) <hewu9375@gmail.com>
2021-11-22 20:16:58 +0900

    Day93: position()

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,38 +7,11 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@heva")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                Image("heva")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("Heva Wu")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello World")
+            .background(.red)
+            .position(x: 100, y: 100)
     }
 }
 
