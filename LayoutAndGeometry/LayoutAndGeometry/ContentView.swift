1a2c952859b0badaa4d41e21f95046c9e98b4cf7
He Wu(Heva) <hewu9375@gmail.com>
2021-11-22 20:37:10 +0900

    Day93: custom coordinate system

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,21 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct OuterView: View {
     var body: some View {
         VStack {
-            GeometryReader { geo in
-                Text("Hello World")
-                    .frame(width: geo.size.width * 0.9, height: 40)
-                    .background(Color.red)
-
-            }
-            .background(.blue)
-            
-            Text("More")
+            Text("Top")
+            InnerView()
                 .background(.green)
+            Text("Bottom")
         }
-        
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
