fec68ac1f6615a67079d6e540d114c1827365767
He Wu(Heva) <hewu9375@gmail.com>
2021-11-29 22:03:01 +0900

    Day96: init project

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by He Wu on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
