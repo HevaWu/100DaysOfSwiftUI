bafa4e4dc1783e939359c69b918a9721cf57cd3b
He Wu(Heva) <hewu9375@gmail.com>
2021-11-29 22:22:04 +0900

    Day96: alert with optional object

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,16 +7,21 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User?
+    
     var body: some View {
-        NavigationView {
-            NavigationLink(destination: Text("New secondary")) {
-                Text("Hello, world!")
+        Text("Hello World")
+            .onTapGesture {
+                selectedUser = User()
             }
-            .navigationBarTitle(Text("Primary"))
-            
-            Text("Secondary")
-        }
+            .alert(item: $selectedUser) { user in
+                Alert(title: Text(user.id))
+            }
     }
 }
 
